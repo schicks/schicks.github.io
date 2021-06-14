b95ba3cd0f910909993debd54ec124cbf64a1773
Sam Schick <schicks448@gmail.com>
2021-06-14 15:25:17 -0400

    whos-simon

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,18 +1,27 @@
 <header>
   <a href="/"><h1>Simon would have said</h1></a>
+  <div class="links">
+    <a href="whos-simon">Who's Simon?</a>
+  </div>
 </header>
 <div class="body">
   <slot />
 </div>
 
-<style>
+<style lang="scss">
   header {
     border-bottom: 8px solid black;
     display: flex;
     align-items: center;
-    padding-left: 1.45rem;
+    justify-content: space-between;
+    padding: 0 1.45rem;
     min-height: 90px;
     margin-bottom: 1.45rem;
+    .links {
+      font-size: 20px;
+      font-weight: 600;
+      margin-top: 10px;
+    }
   }
   .body {
     padding: 0 20px;
