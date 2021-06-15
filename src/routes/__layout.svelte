021fc4d6cfeae282648e632a8cabc0ac2cfcd36c
Sam Schick <schicks448@gmail.com>
2021-06-15 15:54:06 -0400

    more header futzing

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,19 +1,27 @@
 <script>
   import rss from "$lib/icons/rss.svg"
   import home from "$lib/icons/cottage.svg"
+  import github from "$lib/icons/github.svg"
+  import linkedin from "$lib/icons/linkedin.svg"
 </script>
 
 <header>
-  <a href="/" class="home"><h1>Simon would have said</h1></a>
+  <a href="/" class="big"><h1>Simon would have said</h1></a>
   <div class="links">
-    <a href="/" class="home">
+    <a href="/whos-simon" class="big">
+      Who's Simon?
+    </a>
+    <a href="/" class="small">
       <img src={home} alt="Back to index"/>
+    </a>
+    <a href="https://github.com/schicks">
+      <img src={github} alt="Github" style="height: 24px"/>
+    </a>
+    <a href="https://www.linkedin.com/in/sam-schick-868ab8ab/">
+      <img src={linkedin} alt="Linkedin" style="height: 24px"/>
     </a>
     <a href="/feed.xml">
       <img src={rss} alt="rss feed"/>
-    </a>
-    <a href="/whos-simon">
-      Who's Simon?
     </a>
   </div>
 </header>
@@ -23,11 +31,11 @@
 
 <style lang="scss">
   header {
-    @media(max-width:610px) {
-      &>.home {
+    @media(max-width:680px) {
+      .big {
         display: none;
       }
-      .links .home {
+      .small {
         display: inline!important;
       }
     }
@@ -44,22 +52,21 @@
       margin-top: 10px;
       img {
         position: relative;
-        top: 5px;
+        top: 7px;
       }
       a {
-        padding : 0 5px;
+        padding : 0 5px 5px 0;
         border-right: 2px solid black;
         margin: 0;
         text-decoration: none;
         &:hover {
           border-bottom: 2px solid black;
-          border-left: 2px solid black;
         }
         &:last-child {
           border-right: 0;
         }
       }
-      .home {
+      .small {
         display: none;
       }
     }
