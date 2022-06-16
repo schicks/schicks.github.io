66f09147fd6427e26b56f2dfc05ae9d1a627d54c
Sam Schick <schicksw@protonmail.com>
2022-06-16 15:57:17 -0400

    req announcement

--- svelte.config.js
+++ svelte.config.js
@@ -21,9 +21,8 @@ const config = {
 
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte',
     adapter: adapter(),
-    router: false,
+    browser: { router: false },
     vite: {
       build: {
         minify: 'esbuild'
