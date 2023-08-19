f4989d59413ab4ebe5a368130ec55ea4a0cec953
sam schick <github@schicksw.mozmail.com>
2023-08-19 16:41:08 +0000

    updated resume

---
+++ src/lib/Job.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  export let location: string
+</script>
+
+<section>
+  <summary>
+    <h3><slot name="title" /></h3>
+    <aside>{location}</aside>
+  </summary>
+  <slot />
+</section>
+
+<style lang="scss">
+  h3 {
+    margin: 0;
+    font-style: normal;
+  }
+  summary {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 4px;
+  }
+  aside {
+    font-weight: bold;
+  }
+</style>
