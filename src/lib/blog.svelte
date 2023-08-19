6097762250eaf19d25bd91c0e7abc2a3a002585a
sam schick <github@schicksw.mozmail.com>
2023-08-19 18:23:23 +0000

    add comments

--- src/lib/blog.svelte
+++ src/lib/blog.svelte
@@ -3,12 +3,24 @@
   import 'katex/dist/katex.min.css'
   /** @type string */
   export let title
+  const commentProps = {
+    repo: 'schicks/schicks.github.io',
+    'issue-term': title,
+    label: 'blog-comments',
+    theme: 'preferred-color-scheme',
+    crossorigin: 'anonymous',
+    async: true
+  }
 </script>
 
 <article class="blog">
   <h2>{title}</h2>
   <slot />
 </article>
+
+{#if true}
+  <script src="https://utteranc.es/client.js" {...commentProps}></script>
+{/if}
 
 <style global>
   article.blog ol li {
