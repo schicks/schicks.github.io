6d8383ec3a260ee165795944b4c13f40438584d4
sam schick <github@schicksw.mozmail.com>
2025-07-25 20:43:09 -0400

    Remove accidentally committed .svelte-kit files and fix .gitignore
    - Changed /.svelte-kit to **/.svelte-kit in .gitignore to properly ignore nested .svelte-kit directories
    - Removed all previously committed .svelte-kit files from tracking

--- packages/website/.svelte-kit/output/client/_app/immutable/chunks/1-15c1f094.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/error.svelte-09da2ee4.js";export{t as component};
--- packages/website/.svelte-kit/output/client/_app/immutable/chunks/5-3ba8f099.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/resume/_page.svelte-fdd02205.js";export{t as component};
